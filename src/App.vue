cc41b2beb809dd7102a151fd112586f901cd6110
Roger <worfandsheep@qq.com>
2017-06-01 22:37:14 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,40 @@
 <template>
-  <div>
-    <h1>Hello world!</h1>
-
-    <div v-for="level in data">
-      <h2>{{level.name}}</h2>
-      <div v-for="poem in level.poems">
-        <label>
-          <input type="checkbox" v-model="poem.isSelect">
-          <span>{{poem.name}}</span>
-        </label>
+  <div class="warp">
+    <div class="container">
+      <div class="content-header">
+        <h1>古诗文填空自动生成系统</h1>
       </div>
-    </div>
 
-    <div>
-      <textarea class="Result" v-model="result"></textarea>
-      <button @click="generateTest">生成</button>
+      <div class="content">
+        <div class="box box-default" v-for="level in data">
+          <div class="box-header with-border">
+            <h3 class="box-title">{{level.name}}</h3>
+          </div>
+
+          <div class="box-body">
+            <div v-for="poem in level.poems">
+              <label>
+                <input type="checkbox" v-model="poem.isSelect">
+                <span>{{poem.name}}</span>
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div class="box box-default">
+          <div class="box-header with-border">
+            <h3 class="box-title">结果：</h3>
+          </div>
+
+          <div class="box-body">
+            <textarea class="Result" v-model="output"></textarea>
+            <button @click="generate('front')">生成前句填充</button>
+            <button @click="generate('back')">生成后句填充</button>
+            <button @click="generate('mixin')">生成混合填充</button>
+            <button @click="clear()">清空</button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -38,7 +58,7 @@ export default {
   data() {
     return {
       data,
-      result: ''
+      result: []
     }
   },
   computed: {
@@ -52,23 +72,44 @@ export default {
       }
 
       return poems;
+    },
+
+    output() {
+      let output = '';
+
+      for(const poem of this.result) {
+        output += `${poem.quest}\r\n`;
+      }
+
+      return output;
     }
   },
   methods: {
-    generateTest() {
-      this.result = '';
-
+    generate(type) {
       for(const poem of this.poems) {
-        if(poem.isSelect === true) {
-          this.result = `${this.result}\r\n${poem.content}`
+        const isPoemUsed = this.result.some(result => result.name === poem.name);
+//        const isPoemSuitable = poem;
+
+        if(poem.isSelect === true && !isPoemUsed) {
+//          const quest = poem.content;
+          this.result.push({...poem, quest: poem.content});
         }
       }
+    },
+
+    clear() {
+      this.result = [];
     }
   }
 }
 </script>
 
 <style>
+  .warp {
+    background-color: #ecf0f5;
+    min-height: 100vh;
+    min-width: 100vw;
+  }
   .Result {
     width: 100%;
     height: 300px;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import './assets/style/bootstrap.css'
+import './assets/style/AdminLTE.css'
 import Vue from 'vue'
 import App from './App'
 
